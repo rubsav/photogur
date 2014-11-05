65e3b5b611c24798f4c3c51a35b8497c6383c87b
Ruben Avila <Ruavilaa@hotmail.com>
2014-11-04 19:28:55 -0500

    Changing db to pg in production

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,8 @@ Rails.application.routes.draw do
     # get 'pictures/:id' => 'pictures#show', as: 'picture'
     # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
     # patch 'pictures/:id' => "pictures#update"
-    # delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
-  }
-
+    # delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"}
+end
 #-------------------------------------------------------------------------------------
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -66,4 +65,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
